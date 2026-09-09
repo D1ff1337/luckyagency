91e0dc5a809688bc56b9ee837b63852975ee6f9c
D1ff1337 <75628734+D1ff1337@users.noreply.github.com>
2026-09-09 17:24:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -129,7 +129,7 @@ const io = new IntersectionObserver((entries) => {
       io.unobserve(entry.target);
     }
   });
-}, { threshold: 0.12 });
+}, { threshold: 0, rootMargin: '0px 0px -10% 0px' });
 revealEls.forEach(el => io.observe(el));
 
 // ==================== ANIMATED STAT COUNTERS ====================
